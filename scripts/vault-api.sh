70bd96d5d729190383c6204a408fdf3713b9c001
Lagutin Daniil <ldv20u583@student.bmstu.ru>
2024-12-13 20:34:17 +0300

    Check variable expansion 13

--- scripts/vault-api.sh
+++ scripts/vault-api.sh
@@ -1,23 +1,20 @@
 #! /bin/bash
 
 function login_jwt {
-    echo --data "{\"jwt\": \"$1\", \"role\": \"${VAULT_AUTH_ROLE}\"}"
     curl --request POST \
     --data "{\"jwt\": \"$1\", \"role\": \"${VAULT_AUTH_ROLE}\"}" \
-    ${VAULT_SERVER_URL}/v1/auth/jwt/login \
-    | tee test.txt \
+    ${VAULT_SERVER_URL}/v1/auth/jwt/login 2>/dev/null\
     | jq -r ".auth.client_token"
 }
 
 function general {
     curl --header "X-Vault-Token:$1" \
-        "${VAULT_SERVER_URL}$2"
+        "${VAULT_SERVER_URL}$2" 2>/deb/null
 }
 
 function get_kv {
     curl --header "X-Vault-Token:$1" \
-        "${VAULT_SERVER_URL}/v1/$2" \
-        | tee test.txt \
+        "${VAULT_SERVER_URL}/v1/$2" 2>/dev/null \
         | jq -r ".data.$3"
 }
 
