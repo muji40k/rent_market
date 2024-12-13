43b9386f4f52aee83970dd93023c9db728de3199
Lagutin Daniil <ldv20u583@student.bmstu.ru>
2024-12-13 18:12:49 +0300

    Check variable expansion 7

--- scripts/get-email-credentials.sh
+++ scripts/get-email-credentials.sh
@@ -4,14 +4,14 @@ function get {
     get_kv "$VAULT_TOKEN" "test/email/$1" "$2"
 }
 
-export EMAIL_2FA_HOST="$(get server host)"
-export EMAIL_2FA_PORT="$(get server smtp)"
-export EMAIL_2FA_SENDER="$(get sender name)"
-export EMAIL_2FA_SENDER_PASSWORD="$(get sender password)"
-export EMAIL_2FA_SENDER_EMAIL="$(get sender email)"
+echo export EMAIL_2FA_HOST="$(get server host)"
+echo export EMAIL_2FA_PORT="$(get server smtp)"
+echo export EMAIL_2FA_SENDER="$(get sender name)"
+echo export EMAIL_2FA_SENDER_PASSWORD="$(get sender password)"
+echo export EMAIL_2FA_SENDER_EMAIL="$(get sender email)"
 
-export TEST_IMAP_PORT="$(get server imap)"
-export TEST_RECEPIENT_EMAIL="$(get recepient email)"
-export TEST_RECEPIENT_USERNAME="$(get recepient name)"
-export TEST_RECEPIENT_PASSWORD="$(get recepient password)"
+echo export TEST_IMAP_PORT="$(get server imap)"
+echo export TEST_RECEPIENT_EMAIL="$(get recepient email)"
+echo export TEST_RECEPIENT_USERNAME="$(get recepient name)"
+echo export TEST_RECEPIENT_PASSWORD="$(get recepient password)"
 
